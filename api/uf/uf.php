2bc93c5b65e75c7889346b55fcf94afe063710f1
Fabrício Silva <fabriciosilva187@gmail.com>
2023-08-23 01:52:05 -0300

    Uf tratamento de erro

--- api/uf/uf.php
+++ api/uf/uf.php
@@ -4,6 +4,8 @@
 if($api == 'uf'){
 	if($method == "GET"){
 
+
+
 		$db = new DB(); 
 		parse_str($_SERVER['QUERY_STRING'], $parameters);
 
@@ -12,25 +14,20 @@ if($api == 'uf'){
           $parametros  = $recursos[1]; 
 
           //var_dump($recursos);
-
           $rs = $db ->connect()->prepare($sql);
           $rs->execute($parametros);
 
           $error= $rs->errorInfo();
           //var_dump($error);
          // var_dump($rs->debugDumpParams());
- 
+          
+          
           $obj =  $rs-> fetchAll(PDO::FETCH_ASSOC);
-
-          //var_dump(count($obj));
-          // if (count($obj) == 1 && is_null($parametros['status'])) {
-          // 	echo "teste";
-          // }
 
 
           //var_dump($obj[0]['codigo_uf']);
 
-             $result = array_map(function ($data) {
+          $result = array_map(function ($data) {
           	return [
           		'codigoUF'       => $data['codigo_uf'],
           		'sigla'     => $data['sigla'],
@@ -40,74 +37,78 @@ if($api == 'uf'){
           }, $obj);
 
 
-     
-
-
           if((count($obj) == 1 && is_null($parametros['status'])) ||
           	!is_null($parametros['codigoUF'])||
-            !is_null($parametros['sigla']) ||
-            !is_null($parametros['nome'])){
+          	!is_null($parametros['sigla']) ||
+          	!is_null($parametros['nome'])){
           	if(count($obj) > 0){
 
           		$umRegistro = [
-          		'codigoUF'       => $obj[0]['codigo_uf'],
-          		'sigla'     => $obj[0]['sigla'],
-          		'nome' => $obj[0]['nome'],
-          		'status'    => $obj[0]['status'],
-          	];
+          			'codigoUF'       => $obj[0]['codigo_uf'],
+          			'sigla'     => $obj[0]['sigla'],
+          			'nome' => $obj[0]['nome'],
+          			'status'    => $obj[0]['status'],
+          		];
 
-          	   echo json_encode($umRegistro);
+          		echo json_encode($umRegistro);
 
 
           	} 
-          	  
-            }else if(!is_null($parametros['status'])){
-          		    echo json_encode($result);
+          	
+          }else if(!is_null($parametros['status'])){
+          	echo json_encode($result);
 
-          	}
+          }
 
-   
 
+          if($error[0] !="00000"){
+          	echo json_encode(array("mensagem" => "Não foi possível consultar UF no banco de dados.", "status" => 404));
+          	http_response_code(404); exit;
+
+          }
 
           if(count($parametros) == 0){
           	echo json_encode($result);
 
-          } else if(count($result) == 0 || count($parametros) == 0){
+          } 
+
+          else if(count($result) == 0 || count($parametros) == 0){
           	echo json_encode([  ]);
 
           } 
+
 
       }
 
   }
 
-function gerarSQLConsultarListar($params){
-	$parametros  = [];
-	$sql = 'SELECT CODIGO_UF, SIGLA, NOME, STATUS FROM TB_UF WHERE 1 = 1 ';
-	unset($params['path']);
+  function gerarSQLConsultarListar($params){
+  	$parametros  = [];
+  	$sql = 'SELECT CODIGO_UF, SIGLA, NOME, STATUS FROM TB_UF WHERE 1 = 1 ';
+  	unset($params['path']);
     //print_r($params);
 
-	if(!is_null($params['codigoUF'])){
-		$sql .= ' AND CODIGO_UF = :codigoUF ';
-	}
+  	if(!is_null($params['codigoUF'])){
+  		$sql .= ' AND CODIGO_UF = :codigoUF ';
+  	}
 
-	if(!is_null($params['sigla']))
-	{
-		$sql .= " AND SIGLA = :sigla ";
+  	if(!is_null($params['sigla']))
+  	{
+  		$sql .= " AND SIGLA = :sigla ";
 
-	}
-	if(!is_null($params['nome']))
-	{
-		$sql .= ' AND nome = :nome ';
-	}
-	if(!is_null($params['status']))
-	{
-		$sql .= "AND STATUS = :status ";
+  	}
+  	if(!is_null($params['nome']))
+  	{
+  		$sql .= ' AND nome = :nome ';
+  	}
+  	if(!is_null($params['status']))
+  	{
+  		$sql .= "AND STATUS = :status ";
 
-	}
-	$sql .= " ORDER BY CODIGO_UF DESC ";
-	return [$sql, $params];   
-}
+  	}
+  	$sql .= " ORDER BY CODIGO_UF DESC ";
+  	return [$sql, $params];   
+  }
 
 
 
