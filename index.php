b2d68f904923a89175dfb91d719dccf6708e947a
Fabrício Silva <fabriciosilva187@gmail.com>
2023-08-25 16:46:43 -0300

    Exclusão lógica uf

--- index.php
+++ index.php
@@ -8,8 +8,7 @@
 	if(isset($_GET['path']))  {$path = explode("/", $_GET['path']);} else {echo "Caminho não existe"; exit;}
 
 	if(isset($path[0])) {$api = $path[0];} else {echo "Caminho não existe"; exit;}
-	if(isset($path[1])) {$acao = $path[1];} else { $acao = '';}
-	if(isset($path[2])) {$param = $path[2];} else { $param = '';}
+	if(isset($path[1])) {$param = $path[1];} else { $param = '';}
 
 	$method = $_SERVER['REQUEST_METHOD'];
 
